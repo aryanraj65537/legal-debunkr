6b7cc53b454006acac12bbb6758f6ab20e3f919d
aryanraj65537 <ary.r.1103@gmail.com>
2025-03-09 04:24:22 -0400

    Ignore config.js to keep API keys private

--- config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyB8n1pG5nTlFXIGIzaym8GtChnTTjuJO7k",
-    authDomain: "legaldebunkr-8d8d4.firebaseapp.com",
-    projectId: "legaldebunkr-8d8d4",
-    storageBucket: "legaldebunkr-8d8d4.firebasestorage.app",
-    messagingSenderId: "845241159349",
-    appId: "1:845241159349:web:9c11989e780d530bda78fc",
-    measurementId: "G-5QWCKYD6GN",
-};
